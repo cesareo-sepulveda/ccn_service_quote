2f22bc3fbbb980bcfcba99013cbeea855ecd7b68
Cesareo Sepulveda <csepulveda@witann.com>
2025-09-05 03:30:53 +0000

    18.0.8.7.5 - Filtrado por cliente

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "CCN Service Quote",
     "summary": "Wizard para cotizar servicios CCN",
-    "version": "18.0.8.7.4",
+    "version": "18.0.8.7.5",
     "author": "Witann Technologies",
     "license": "LGPL-3",
     "category": "Sales/Sales",
