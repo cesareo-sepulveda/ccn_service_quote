1447bc403a4cfa18ee3ab331470a090b1a9f3212
Cesareo Sepulveda <csepulveda@witann.com>
2025-09-05 08:23:40 +0000

    18.0.8.7.12 - Fix en seleccion de productos service_quote

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "CCN Service Quote",
     "summary": "Wizard para cotizar servicios CCN",
-    "version": "18.0.8.7.11",
+    "version": "18.0.8.7.12",
     "author": "Witann Technologies",
     "license": "LGPL-3",
     "category": "Sales/Sales",
--- models/product.py
+++ models/product.py
@@ -2,21 +2,16 @@
 from odoo import models, fields
 
 class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+    _inherit = "product.template"
 
-    # Tags de Rubro usados para filtrar en CCN Service Quote
     ccn_rubro_ids = fields.Many2many(
-        'ccn.service.rubro',
-        'ccn_rubro_product_rel',
-        'product_tmpl_id',
-        'rubro_id',
-        string='Rubros CCN',
-        help='Rubros CCN a los que pertenece este producto/servicio.'
+        "ccn.rubro",
+        "product_tmpl_rubro_rel",
+        "product_tmpl_id",
+        "rubro_id",
+        string="Rubros (CCN)"
     )
-
-    # Ocultar este producto del selector en CCN Service Quote (para “contenedores”)
     ccn_exclude_from_quote = fields.Boolean(
-        string='Ocultar en CCN Service Quote',
-        default=False,
-        help='Si está activo, este producto no aparece al elegir productos en CCN Service Quote.'
+        string="Excluir de CCN Service Quote",
+        help="Si está activo, este producto no aparecerá en el selector de líneas CCN."
     )
