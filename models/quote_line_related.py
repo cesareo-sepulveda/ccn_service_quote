46956313e34db6f05d4d3878fbc1434f283471e4
cesareo-sepulveda <64621397+cesareo-sepulveda@users.noreply.github.com>
2025-09-08 03:09:21 -0600

    Remove incompatible product_base_price field

--- models/quote_line_related.py
+++ models/quote_line_related.py
@@ -3,14 +3,6 @@ from odoo import models, fields
 class ServiceQuoteLineRelated(models.Model):
     _inherit = 'ccn.service.quote.line'
 
-    # Precio unitario desde el TEMPLATE para mantener tipo Monetary correcto
-    product_base_price = fields.Monetary(
-        string='Precio unitario',
-        related='product_id.product_tmpl_id.list_price',
-        currency_field='currency_id',
-        readonly=True,
-    )
-
     # Impuestos del producto (para mostrar en la lista)
     product_taxes_id = fields.Many2many(
         comodel_name='account.tax',
