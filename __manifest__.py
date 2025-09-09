953410a26a06e28a3048210b9548ce0ff1bf2968
cesareo-sepulveda <64621397+cesareo-sepulveda@users.noreply.github.com>
2025-09-08 18:15:08 -0600

    18.0.9.1.28 - Fix left chevron orientation and tab colors

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "CCN Service Quote",
     "summary": "Wizard para cotizar servicios CCN",
-    "version": "18.0.9.1.27",
+    "version": "18.0.9.1.28",
     "author": "Witann Technologies",
     "license": "LGPL-3",
     "category": "Sales/Sales",
