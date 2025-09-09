bbc99aaff5b2b598624e3fe7404a3de9c19faaed
Cesareo Sepulveda <csepulveda@witann.com>
2025-09-09 09:49:05 +0000

    18.0.9.2.4 - Forced assets loading 3

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "CCN Service Quote",
     "summary": "Wizard para cotizar servicios CCN",
-    "version": "18.0.9.2.3",
+    "version": "18.0.9.2.4",
     "author": "Witann Technologies",
     "license": "LGPL-3",
     "category": "Sales/Sales",
