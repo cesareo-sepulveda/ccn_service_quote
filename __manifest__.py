52ccfee4275d54ccf3486dedcfa09beef845f886
Cesareo Sepulveda <csepulveda@witann.com>
2025-09-09 12:22:17 +0000

    18.0.9.2.18 - Fix tab logic

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "CCN Service Quote",
     "summary": "Wizard para cotizar servicios CCN",
-    "version": "18.0.9.2.17",
+    "version": "18.0.9.2.18",
     "author": "Witann Technologies",
     "license": "LGPL-3",
     "category": "Sales/Sales",
