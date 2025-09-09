9a0edbe6a57d46805cf378c0a4f3d854c89d9349
Cesareo Sepulveda <csepulveda@witann.com>
2025-09-09 11:19:25 +0000

    18.0.9.2.11 - Propagar estado de clases

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "CCN Service Quote",
     "summary": "Wizard para cotizar servicios CCN",
-    "version": "18.0.9.2.10",
+    "version": "18.0.9.2.11",
     "author": "Witann Technologies",
     "license": "LGPL-3",
     "category": "Sales/Sales",
