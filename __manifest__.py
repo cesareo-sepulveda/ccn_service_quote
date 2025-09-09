f335bd0f3c584e4f7931bfc6da4138646ddaa07c
Cesareo Sepulveda <csepulveda@witann.com>
2025-09-09 00:54:09 +0000

    18.0.9.1.33 - Fix css

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "CCN Service Quote",
     "summary": "Wizard para cotizar servicios CCN",
-    "version": "18.0.9.1.32",
+    "version": "18.0.9.1.33",
     "author": "Witann Technologies",
     "license": "LGPL-3",
     "category": "Sales/Sales",
