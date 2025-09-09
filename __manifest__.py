2c805ab949277bed35aa3a0ddb92efe1a716fa3e
Cesareo Sepulveda <csepulveda@witann.com>
2025-09-09 13:54:53 +0000

    18.0.9.2.29 - Fix separate listing per rubro

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "CCN Service Quote",
     "summary": "Wizard para cotizar servicios CCN",
-    "version": "18.0.9.2.28",
+    "version": "18.0.9.2.29",
     "author": "Witann Technologies",
     "license": "LGPL-3",
     "category": "Sales/Sales",
