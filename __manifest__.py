9af4dad40a43c1bf69d3c944eea69dfb70de2a24
Cesareo Sepulveda <csepulveda@witann.com>
2025-09-09 11:07:11 +0000

    18.0.9.2.10 - fix classes loading

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "CCN Service Quote",
     "summary": "Wizard para cotizar servicios CCN",
-    "version": "18.0.9.2.9",
+    "version": "18.0.9.2.10",
     "author": "Witann Technologies",
     "license": "LGPL-3",
     "category": "Sales/Sales",
