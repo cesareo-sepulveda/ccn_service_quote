c9ac8e1638e821e6db3ae736e2fc5cce85975b5f
Cesareo Sepulveda <csepulveda@witann.com>
2025-09-09 14:42:52 +0000

    18.0.9.2.36 - Fix chevron

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "CCN Service Quote",
     "summary": "Wizard para cotizar servicios CCN",
-    "version": "18.0.9.2.35",
+    "version": "18.0.9.2.36",
     "author": "Witann Technologies",
     "license": "LGPL-3",
     "category": "Sales/Sales",
