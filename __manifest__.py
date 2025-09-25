6b78756e10363ae69b97162b3edbf315823488cd
Cesareo Sepulveda <csepulveda@witann.com>
2025-09-25 16:26:39 +0000

    18.0.9.3.143 - Fix informacion repetida en rubros

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "Cotizador Especial CCN",
     "summary": "Wizard para cotizar servicios CCN",
-    "version": "18.0.9.3.142",
+    "version": "18.0.9.3.143",
     "author": "Witann Technologies",
     "license": "LGPL-3",
     "category": "Sales/Sales",
