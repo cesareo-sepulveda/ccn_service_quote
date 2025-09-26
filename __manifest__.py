e399f72ec13f166a7964b77d3504e290a8d6b7e3
Cesareo Sepulveda <csepulveda@witann.com>
2025-09-26 16:59:07 +0000

    18.0.9.4.8

--- __manifest__.py
+++ __manifest__.py
@@ -2,31 +2,46 @@
 {
     "name": "Cotizador Especial CCN",
     "summary": "Wizard para cotizar servicios CCN",
-    "version": "18.0.9.4.7",
+    "version": "18.0.9.4.8",
     "author": "Witann Technologies",
     "license": "LGPL-3",
     "category": "Sales/Sales",
     "depends": ["base", "web", "product", "sale", "account"],
     "data": [
+        # Seguridad siempre primero
         "security/ir.model.access.csv",
+
+        # Limpiezas / fixes de arranque
         "data/cleanup_views.xml",
+
+        # 🔴 IMPORTANTE: cargar PRIMERO la vista base de líneas inline
+        "views/quote_line_list_inline.xml",
+
+        # Luego cualquier vista que herede/ajuste esa base
+        "views/ccn_quote_line_inline_product_domain.xml",
+
+        # Vistas principales
         "views/quote_views.xml",
         "views/ccn_quote_form_scope.xml",
         "views/quote_site_fix.xml",
-        "views/ccn_quote_line_inline_product_domain.xml",
+
+        # Datos y catálogos
         "data/rubro_data.xml",
         "views/rubro_views.xml",
-        "views/quote_line_list_inline.xml",
+        "data/ccn_rubros.xml",
+
+        # Otras vistas/acciones/menus
         "views/pick_quote_wizard.xml",
         "views/quote_actions.xml",
         "views/ccn_menus.xml",
         "views/sale_order_button.xml",
         "views/product_template_ccn.xml",
-        "data/ccn_rubros.xml",
         "views/site_views.xml",
         "views/quote_tabs_status.xml",
         "views/cleanup_disable_legacy_views.xml",
         "views/res_partner_views.xml",
+
+        # Migración / normalización (idempotente)
         "data/migrate_fix_general.xml",
     ],
     "assets": {
