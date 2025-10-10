be0b811a3f1dea1255a528b28f9e06ec0549fd9e
Cesareo Sepulveda <csepulveda@witann.com>
2025-10-10 11:47:34 +0000

    18.0.9.8.44 - Notebooks con columnas definitivas y computadas

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "Cotizador Especial CCN",
     "summary": "Wizard para cotizar servicios CCN",
-    "version": "18.0.9.8.41",
+    "version": "18.0.9.8.44",
     "author": "Witann Technologies",
     "license": "LGPL-3",
     "category": "Sales/Sales",
