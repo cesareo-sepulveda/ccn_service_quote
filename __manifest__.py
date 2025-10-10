7fe4ecb142fc01db5980ef3d641711e0d9e63a27
Cesareo Sepulveda <csepulveda@witann.com>
2025-10-10 11:50:18 +0000

    18.0.9.8.45 - Ajuste columna puesto en Mano de obra

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "Cotizador Especial CCN",
     "summary": "Wizard para cotizar servicios CCN",
-    "version": "18.0.9.8.44",
+    "version": "18.0.9.8.45",
     "author": "Witann Technologies",
     "license": "LGPL-3",
     "category": "Sales/Sales",
