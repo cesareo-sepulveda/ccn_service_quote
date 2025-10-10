b6f3cf664b364c3dc0022772771cd184276da01e
Cesareo Sepulveda <csepulveda@witann.com>
2025-10-10 07:33:51 +0000

    18.0.9.8.25 - Outline

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 {
     "name": "Cotizador Especial CCN",
     "summary": "Wizard para cotizar servicios CCN",
-    "version": "18.0.9.8.24",
+    "version": "18.0.9.8.25",
     "author": "Witann Technologies",
     "license": "LGPL-3",
     "category": "Sales/Sales",
